d856097d5e06265b93db6aff9bdc8f14462f5ae7
Georg Ledermann <georg@ledermann.dev>
2023-11-22 07:37:26 +0100

    :arrow_up: yarn upgrade

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +10,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  }
+  },
 });
